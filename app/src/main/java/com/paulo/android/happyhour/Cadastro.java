aa63fc11fbcfa28413a8e9732cafe4cd92ae1309
Paulo Henrique <paulo.13.102@hotmail.com>
2016-10-21 00:07:57 -0200

    Ajustes

--- app/src/main/java/com/paulo/android/happyhour/Cadastro.java
+++ app/src/main/java/com/paulo/android/happyhour/Cadastro.java
@@ -2,11 +2,11 @@ package com.paulo.android.happyhour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Cadastro extends AppCompatActivity {
-
-    EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +14,21 @@ public class Cadastro extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
 
         EditText email = (EditText) findViewById(R.id.email2);
-        senha = (EditText) findViewById(R.id.password2);
+        EditText senha = (EditText) findViewById(R.id.password2);
 
         String resultEmail = getIntent().getStringExtra("Email");
         String resultPassword = getIntent().getStringExtra("Password");
 
         email.setText(resultEmail);
         senha.setText(resultPassword);
+
+        Button cancelar = (Button) findViewById(R.id.cancelar);
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
