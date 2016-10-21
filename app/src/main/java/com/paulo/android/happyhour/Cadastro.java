13f1d73919537af8d59203e93dea1fcd30a583f0
Paulo Henrique <paulo.13.102@hotmail.com>
2016-10-20 23:40:39 -0200

    Passando dados

--- app/src/main/java/com/paulo/android/happyhour/Cadastro.java
+++ app/src/main/java/com/paulo/android/happyhour/Cadastro.java
@@ -2,12 +2,24 @@ package com.paulo.android.happyhour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Cadastro extends AppCompatActivity {
+
+    EditText senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        EditText email = (EditText) findViewById(R.id.email2);
+        senha = (EditText) findViewById(R.id.password2);
+
+        String resultEmail = getIntent().getStringExtra("Email");
+        String resultPassword = getIntent().getStringExtra("Password");
+
+        email.setText(resultEmail);
+        senha.setText(resultPassword);
     }
 }
--- app/src/main/java/com/paulo/android/happyhour/activities/LoginCadastro.java
+++ app/src/main/java/com/paulo/android/happyhour/activities/LoginCadastro.java
@@ -5,23 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.paulo.android.happyhour.Cadastro;
 import com.paulo.android.happyhour.R;
 
 public class LoginCadastro extends AppCompatActivity {
 
+    EditText email;
+    EditText senha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_cadastro);
 
+        email = (EditText) findViewById(R.id.email1);
+        senha = (EditText) findViewById(R.id.password1);
         Button cadastrar = (Button) findViewById(R.id.cadastrar);
+
+
 
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginCadastro.this, Cadastro.class);
+                intent.putExtra("Email", email.getText().toString());
+                intent.putExtra("Password", senha.getText().toString());
                 startActivity(intent);
             }
         });
