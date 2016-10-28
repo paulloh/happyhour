cbb5f471a665a1908e617d328e45ee471e5cf11a
Paulo Henrique <paulo.13.102@hotmail.com>
2016-10-28 21:52:31 -0200

    Ajustes

--- app/src/main/java/com/paulo/android/happyhour/Cadastro.java
+++ app/src/main/java/com/paulo/android/happyhour/Cadastro.java
@@ -1,11 +1,18 @@
 package com.paulo.android.happyhour;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -14,7 +21,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Cadastro extends AppCompatActivity {
+public class Cadastro extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
+
+    private EditText email;
+    private EditText senha;
+    private EditText senha2;
 
     private DatabaseReference mDataBase;
     private FirebaseAuth mAuth;
@@ -25,42 +38,113 @@ public class Cadastro extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
 
         mDataBase = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
 
-        final EditText email = (EditText) findViewById(R.id.email2);
-        final EditText senha = (EditText) findViewById(R.id.password2);
+         email = (EditText) findViewById(R.id.email2);
+         senha = (EditText) findViewById(R.id.password2);
+         senha2 = (EditText) findViewById(R.id.confirmPass);
+
+        findViewById(R.id.cadastrar2).setOnClickListener(this);
+        findViewById(R.id.cancelar).setOnClickListener(this);
 
         final String resultEmail = getIntent().getStringExtra("Email");
         final String resultPassword = getIntent().getStringExtra("Password");
 
+        mAuth = FirebaseAuth.getInstance();
+
         email.setText(resultEmail);
         senha.setText(resultPassword);
 
-        Button cancelar = (Button) findViewById(R.id.cancelar);
-        Button cadastrar = (Button) findViewById(R.id.cadastrar);
-
-//        cadastrar.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                mAuth.createUserWithEmailAndPassword(resultEmail, resultPassword)
-//                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<AuthResult> task) {
-//                                if (task.isSuccessful()){
-//
-//                                }
-//                            }
-//                        })
-//            }
-//        });
-
-
-        cancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
     }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Cadastro.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(Cadastro.this, R.string.sign_up_success,
+                                    Toast.LENGTH_SHORT).show();
+                            Intent inte = new Intent(Cadastro.this, MainActivity.class);
+                            startActivity(inte);
+                            createNotification();
+                            finish();
+                        }
+
+                    }
+                });
+        // [END create_user_with_email]
+        }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String mail = email.getText().toString();
+        if (TextUtils.isEmpty(mail)) {
+            email.setError("O campo E-mail é obrigatório.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String password = senha.getText().toString();
+        String confirmPass = senha2.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            senha.setError("O campo senha é obrigatório.");
+            valid = false;
+        } else {
+            senha.setError(null);
+        }
+
+        if(TextUtils.isEmpty(confirmPass)){
+            senha2.setError("Favor confirmar senha digitada.");
+            valid = false;
+        }
+
+        if (!TextUtils.equals(password, confirmPass)){
+            senha2.setError("As senhas digitadas não são iguais!");
+            valid = false;
+        }
+        return valid;
+    }
+
+    public void createNotification(){
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pIntente = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), i, 0);
+
+        Notification noti = new Notification.Builder(this).setContentTitle("Cadastro realizado com sucesso")
+                .setContentText("Bem Vindo ao HAPPY HOUR!")
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
+                .build();
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+        manager.notify(0, noti);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.cadastrar2) {
+            createAccount(email.getText().toString(), senha.getText().toString());
+            finish();
+        }else if(i == R.id.cancelar){
+            finish();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/paulo/android/happyhour/activities/LoginActivity.java
@@ -0,0 +1,108 @@
+package com.paulo.android.happyhour.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.paulo.android.happyhour.Cadastro;
+import com.paulo.android.happyhour.MainActivity;
+import com.paulo.android.happyhour.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText email;
+    private EditText senha;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email1);
+        senha = (EditText) findViewById(R.id.password1);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.cadastrar).setOnClickListener(this);
+        findViewById(R.id.entrar).setOnClickListener(this);
+    }
+
+    private void login(String email, String senha){
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Usuário ou senha inválidos!", Toast.LENGTH_LONG).show();
+                        }else{
+                            FirebaseUser user = task.getResult().getUser();
+                            if (user != null){
+                                Intent it = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(it);
+                                finish();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void realizarCadastro(){
+        Intent intent = new Intent(LoginActivity.this, Cadastro.class);
+        intent.putExtra("Email", email.getText().toString());
+        intent.putExtra("Password", senha.getText().toString());
+        startActivity(intent);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String mail = email.getText().toString();
+        if (TextUtils.isEmpty(mail)) {
+            email.setError("O campo E-mail é obrigatório.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String password = senha.getText().toString();
+
+        if (TextUtils.isEmpty(password)) {
+            senha.setError("O campo senha é obrigatório.");
+            valid = false;
+        } else {
+            senha.setError(null);
+        }
+
+        return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.entrar) {
+            login(email.getText().toString(), senha.getText().toString());
+            finish();
+        }else if(i == R.id.cadastrar){
+            realizarCadastro();
+            finish();
+        }
+    }
+}
